527da3b48289786b2ba183d5039aadba3b2a7bea
varenyeolad <97789571+varenyeolad@users.noreply.github.com>
2022-05-27 17:54:45 +0600

    sign up

---
+++ public/js/quiz_answers.js
@@ -0,0 +1,3 @@
+let answers ={
+    a
+}
---
+++ public/js/quiz_data.js
@@ -0,0 +1,154 @@
+const quiz =[
+    {
+       " q": "Choose one of two activities",
+        "an" : {
+           "a": "To raise and train service dogs to search for drugs",
+           "b": "Develop new models of electronic household appliances"
+        }
+
+    },
+    {
+        " q": "Choose one of two activities",
+        "an" : {
+            "a": "Rescue people after earthquakes",
+            "b": "Certify documents, issue power of attorney and contracts"
+        }
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "Sing in a music group ",
+            "b": "To train intensively, improve and achieve new sports results "
+        }
+        },
+
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+    {
+        "q": "Choose one of two activities",
+        "an" : {
+            "a": "",
+            "b": ""
+        }
+
+
+    },
+
+
+]
